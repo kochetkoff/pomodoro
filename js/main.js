9416f49b058b3bdbaf9763207983a213430bf876
kochetkoff <artk4@yandex.ru>
2017-04-20 14:51:38 +0300

    startBreakTimer, startWorkTimer functions made

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function() {
     "use strict";
     var MS_IN_MIN = 60000;
     var MS_IN_SEC = 1000;
+
     // Make circle round and position inner in the center of outer
     function positionCircle(){
         var inner = document.getElementById('inner');
@@ -14,20 +15,24 @@ $(function() {
         pointer.style.height = inner.style.top = (outer.offsetWidth - inner.offsetWidth)/2 + "px";
     }
     positionCircle();
+
     // Reposition elements on window resize
     window.onresize = function() {
         positionCircle();
     };
+
     // Check if input in controls is an integer between 1 and 60
     var MIN_INPUT_VAL = 1;
     var MAX_INPUT_VAL = 60;
     function isValidInput(inpValue){
         return !isNaN(parseInt(inpValue))&&(inpValue >= MIN_INPUT_VAL)&&(inpValue <= MAX_INPUT_VAL)&&(Math.floor(inpValue) === +inpValue);
     }
+
     // Validate input on input event
     function validateOnInput() {
         isValidInput(this.value) ? this.classList.remove('input-error') : this.classList.add('input-error');
     }
+
     // Validate input on blur event
     function validateOnBlur() {
         var alertWarning = document.querySelector('.alert-warning');
@@ -42,11 +47,13 @@ $(function() {
     var breakInput = document.getElementById('break__input');
     workInput.oninput = breakInput.oninput = validateOnInput;
     workInput.onblur = breakInput.onblur = validateOnBlur;
+
     // Increment and decrement session and break time
     var workMinus = document.querySelector('#settings__work .decrement');
     var workPlus = document.querySelector('#settings__work .increment');
     var breakMinus = document.querySelector('#settings__break .decrement');
     var breakPlus = document.querySelector('#settings__break .increment');
+
     // Increment and decrement inputs on click
     workMinus.onclick = function () {
         if(!isValidInput(workInput.value)) return;
@@ -73,32 +80,63 @@ $(function() {
     function whatSession() {
         return document.body.getAttribute('data-session');
     }
-    function startSession() {
-        var workPeriod;
-        var breakPeriod;
-        var timeLeft;
-        var startTime;
-        var endTime;
-        workPeriod = parseInt(document.getElementById('work__input').value)*MS_IN_MIN;
-        breakPeriod = parseInt(document.getElementById('break__input').value)*MS_IN_MIN;
 
-        // renders time in circle
-        function renderTime(timeLeft) {
-            var d = new Date(timeLeft);
-            var m = d.getMinutes();
-            var s = d.getSeconds();
-            document.getElementById('timer__min').innerHTML =String(m);
-            document.getElementById('timer__sec').innerHTML = s < 10 ? "0" + s : String(s);
+    // renders time in circle
+    function renderTime(timeLeftSec) {
+        var d = new Date(timeLeftSec*1000);
+        var m = d.getMinutes();
+        var s = d.getSeconds();
+        document.getElementById('timer__min').innerHTML =String(m);
+        document.getElementById('timer__sec').innerHTML = s < 10 ? "0" + s : String(s);
+    }
+
+
+
+    // starts session after setup finished and start clicked
+    function startSession() {
+        var workPeriod = parseInt(document.getElementById('work__input').value)*MS_IN_MIN;
+        var breakPeriod = parseInt(document.getElementById('break__input').value)*MS_IN_MIN;
+
+        // starts work timer
+        function startWorkTimer() {
+            renderTime(workPeriod/1000);
+            var timeLeftSec = workPeriod/1000;
+            var workTimer = setInterval(function(){
+                if (timeLeftSec > 0) {
+                    renderTime(timeLeftSec--)
+                } else {
+                    clearInterval(workTimer);
+                    startBreakTimer();
+                }
+            }, 1000);
+            document.body.setAttribute('data-session', 'work');
         }
 
-        renderTime(workPeriod);
-        document.body.setAttribute('data-session', 'work');
+        // starts break timer
+        function startBreakTimer() {
+            renderTime(breakPeriod/1000);
+            var timeLeftSec = breakPeriod/1000;
+            var breakTimer = setInterval(function(){
+                if (timeLeftSec > 0) {
+                    renderTime(timeLeftSec--)
+                } else {
+                    clearInterval(breakTimer);
+                    startWorkTimer();
+                }
+            }, 1000);
+            document.body.setAttribute('data-session', 'break');
+        }
+
+        // session always starts with work period
+        startWorkTimer();
     }
+
+    // stops timer, work or break session and returns to setup
     function stopSession() {
         document.body.setAttribute('data-session', 'setup');
     }
-
-    inner.onclick = function (e) {
+    // starts or stops session on click
+    inner.onclick = function () {
         if(whatSession() === "setup"){
             startSession();
         } else {
